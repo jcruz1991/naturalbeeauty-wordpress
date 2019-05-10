d1d56b8587acf06a64a781387e57cada9c5011ed
Julio Cruz <atlascruz009@gmail.com>
2019-05-10 11:08:33 -0700

    modified front page

--- front-page.php
+++ front-page.php
@@ -8,6 +8,17 @@
  */
 get_header();
 ?>
+
+<?php
+$id = '93'; // 93 or 11
+$product = wc_get_product($id);
+$product_name = $product->name;
+$product_price = $product->price;
+$product_short_description = $product->short_description;
+$url = get_permalink($id);
+// Get Product Images
+$product_image = get_the_post_thumbnail_url($product->get_id(), 'large');
+?>
 <div id="primary" class="content-area font-primary">
     <main id="landing-main" class="site-main">
         <div class="landing-hero">
@@ -27,7 +38,7 @@ get_header();
                             <div class="landing-text-wrapper">
                                 <h1>Natural Beeauty</h1>
                                 <p>Experience the power for honey. Our all natural honey face wash is infused with salt, royal jelly, collagen and liquid oxygen to help clear acne, hydrate skin, and keep skin looking healthy and youthful!</p>
-                                <a href="product/honey-cleanser-exfoliator/">Buy Now</a>
+                                <a href="<?php echo $url; ?>">Buy Now</a>
                             </div>
                         </div>
                     </div>
@@ -37,15 +48,6 @@ get_header();
         <section class="front-section">
             <div class="front-section-product">
                 <div class="product">
-                    <?php
-                    $id = '93'; // 93 or 11
-                    $product = wc_get_product($id);
-                    $product_name = $product->name;
-                    $product_price = $product->price;
-                    $product_short_description = $product->short_description;
-                    // Get Product Images
-                    $product_image = get_the_post_thumbnail_url($product->get_id(), 'large');
-                    ?>
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-8 col-sm-12">
@@ -59,38 +61,13 @@ get_header();
                                     <div class="divider"></div>
                                     <p class="front-product-description"><?php echo $product_short_description ?></p>
                                     <p class="front-product-price">&#36;<?php echo $product_price ?></p>
-                                    <a href="https://naturalbeeauty.com/product/honey-cleanser-exfoliator/"><button class="front-product-button">View Product</button></a>
+                                    <a href="<?php echo $url; ?>"><button class="front-product-button">View Product</button></a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- <div class="front-page-single-product-wrapper">
-                <h2 class="text-center">Our Product</h2>
-                <?php
-                $id = '93'; // 93 or 11
-                $product = wc_get_product($id);
-                $product_name = $product->name;
-                $product_price = $product->price;
-                $product_short_description = $product->short_description;
-                // Get Product Images
-                $product_image = get_the_post_thumbnail_url($product->get_id(), 'medium');
-                ?>
-                <div class="container">
-                    <div class="front-product-container flex flex-center flex-row">
-                        <div class="front-product-image">
-                            <img src=<?php echo $product_image ?> alt="Product Image">
-                        </div>
-                        <div class="front-product-info">
-                            <h3><?php echo $product_name ?></h3>
-                            <p class="front-product-description"><?php echo $product_short_description ?></p>
-                            <p class="front-product-price">&#36;<?php echo $product_price ?></p>
-                            <a href="https://naturalbeeauty.com/product/honey-cleanser-exfoliator/"><button class="front-product-button">View Product</button></a>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
         </section><!-- END OF SECTION -->
         <!--START OF SECTION -->
         <section class="front-section">
@@ -119,19 +96,26 @@ get_header();
                         <div class="card">
                             <div class="card-body">
                                 <h2>Experience the power of honey</h2>
-                                <p>Learn more about our ingredients.</p>
+                                <p>Learn more about our all natural handmade honey facial scrub.</p>
+                                <a href="<?php echo $url; ?>">View</a>
                             </div>
                         </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <a href="<?php echo get_page_link(get_page_by_title('faq')->ID); ?>">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h2>FAQ</h2>
+                                    <p>Take a look at some of our most frequently ask questions.</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </a>
+                        <a href="<?php echo get_page_link(get_page_by_title('contact')->ID); ?>">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h2>Contact Us</h2>
+                                    <p>Have any questions? Feel free to get in touch with us about any questions you may have!</p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                 </div>
             </div>
